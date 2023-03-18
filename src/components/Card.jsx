a629f90791af37d73163078026cf0adb2b02fead
helmicodes <helmicodes@gmail.com>
2023-03-18 22:16:55 +0800

    add data with props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,17 @@
 import React from "react"
 
-export default function Main() {
+export default function Main(props) {
     return(
         <div className="card">
+            <img
+                src={props.imageUrl}
+                className="card--image"
+            />
+            <div>
+                <p>{props.title}</p>
+                <p>{props.startDate} - {props.endDate}</p>
+                <p>{props.description}</p>
+            </div>
         </div>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,23 @@
 import React from "react"
 import Card from "./Card"
+import data from "../data"
 
 export default function Main() {
+    const cards = data.map((item) => {
+        return <Card
+            title={item.title}
+            location={item.location}
+            googleMapsUrl={item.googleMapsUrl}
+            startDate={item.startDate}
+            endDate={item.endDate}
+            description={item.description}
+            imageUrl={item.imageUrl}
+        />
+    })
+
     return(
         <div className="main">
-            <Card />
+            {cards}
         </div>
     )
 }
